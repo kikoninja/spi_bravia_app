a8f63a89ac8533a15b078682f689a24f80d0ada9
Peco Danajlovski <peco@invideous.com>
2012-04-03 10:18:44 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SPIBraviaApp::Application.config.secret_token = '7bec5804901bacbedf27c323196698e43a0500b5c724a8137e66338ccfea466b51d84a27c9530538b68ca7805e760d61f15e34ff80725dc2c599111670725ce7'
