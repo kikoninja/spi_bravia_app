4eb59df11012c254db353f428f3dc4b05837e2f9
Milan Nasevic <mnasevic@gmail.com>
2012-04-05 08:02:17 +0200

    Added helper methods to build url's for refreshing the feeds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+require 'open-uri'
+require 'digest'
+
+  def make_URL(baseurl, service_name, feed_id, secret_key)
+      url = baseurl + "?service_name=" + URI::encode(service_name) + "&method=fromFeed&feed_id=" + feed_id + "&timestamp=" + URI::encode(makeRFC3339(Time.now))
+      sig = Digest::MD5.hexdigest(url+secret_key)
+      url += "&sig=" + sig
+  end
+
+  def makeRFC3339(timestamp)
+    timestring = timestamp.xmlschema
+  end
+
 end
