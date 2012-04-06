262b7555f056de2c4e51f45d53d7b5cf2873725f
Peco Danajlovski <peco@invideous.com>
2012-04-06 15:02:21 +0200

    introduced feeds and reconfigured the controller to use feed objects

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,10 +2,19 @@ class FeedsController < ApplicationController
 
   respond_to :xml
 
-  def branch
-    @channel = Channel.first
+  # Filters
+  before_filter :load_channel
+
+  def show
+    @feed = Feed.find_by_title(params[:id])
 
     render :content_type => 'application/xml'
+  end
+
+  private
+
+  def load_channel
+    @channel = Channel.first
   end
 
 end
