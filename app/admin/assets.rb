f03e5dd053a1e0cf8e89d59aeebb92f442791d0b
Milan Nasevic <mnasevic@gmail.com>
2012-04-11 16:50:45 +0200

    Added custom form for admin asset form

--- app/admin/assets.rb
+++ app/admin/assets.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Asset do
-  
+
+  form do |f|
+    f.inputs do
+      f.input :feed_id, :label => "Feed", :as => :select, :collection => Feed.order('id ASC')
+      f.input :video_id, :label => "Video", :as => :select, :collection => Video.order('id ASC')
+      f.input :title
+      f.input :description
+      f.input :pay_content, :as => :boolean
+      f.input :asset_type, :label => "Asset Type", :as => :select, :collection => Asset::TYPES
+    end
+    f.buttons
+  end
+
 end
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,7 @@
 class Asset < ActiveRecord::Base
 
+  TYPES = [ "video", "audio", "message", "icon" ]
+
   # Associations
   belongs_to :feed
   belongs_to :video
