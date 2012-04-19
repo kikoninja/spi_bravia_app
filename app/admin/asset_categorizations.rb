ef9a23cf73bd0548dfc6afd9d7a6dd95d03e424f
Milan Nasevic <mnasevic@gmail.com>
2012-04-19 19:56:39 +0200

    Introduced package categories

--- app/admin/asset_categorizations.rb
+++ app/admin/asset_categorizations.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register AssetCategorization do
-  
+
+  form do |f|
+    f.inputs do
+      f.input :asset_id, :label => "Asset", :as => :select, :collection => Asset.order('id ASC')
+      f.input :category_id, :label => "Category", :as => :select, :collection => Category.roots
+    end
+    f.buttons
+  end
+
 end
