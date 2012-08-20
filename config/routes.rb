8aa3b51bdf9cc8f69b654ad780d379a9bc48e05c
Peco Danajlovski <vortexmk@gmail.com>
2012-08-20 15:02:25 +0200

    updated the route for ssm_getuserdata

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ SPIBraviaApp::Application.routes.draw do
   # Matches
   match "/bivldev/sts_get_authorization/STSgetAuthorization", controller: "authorization", action: "sts_get_authorization", format: "xml"
   match "/bivldev/ssm_get_userdata/SSMgetUserData", controller: "authorization", action: "ssm_get_userdata", format: "xml"
+  match "/bivldev/ssm_get_userdata", controller: "authorization", action: "ssm_get_userdata", format: "xml"
   match "/admin", controller: "admin/dashboard", action: "index"
 
   # Root
