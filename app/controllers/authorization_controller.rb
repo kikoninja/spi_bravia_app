df2cee16e5af061154ad1776e417834d979f8ea5
Peco Danajlovski <peco@invideous.com>
2012-08-22 15:45:17 +0200

    moved the code to the right controller

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -3,6 +3,7 @@ class AuthorizationController < ApplicationController
   respond_to :xml
 
   def sts_get_authorization
+    @reg_url = Rails.env == "development" ? "http://dev.internet.sony.tv" : "http://internet.sony.tv"
     @asset_id = params[:id]
 
     render content_type: 'application/xml'
--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -6,7 +6,6 @@ class FeedsController < ApplicationController
   before_filter :load_channel
 
   def show
-    @reg_url = Rails.env == "development" ? "http://dev.internet.sony.tv" : "http://internet.sony.tv"
     @feed = Feed.find_by_title(params[:id])
 
     render :content_type => 'application/xml'
