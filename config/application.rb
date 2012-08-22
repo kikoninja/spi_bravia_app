1acb9d37835af7f44603741ab6685cd6c7bdf41e
Peco Danajlovski <peco@invideous.com>
2012-08-22 16:45:13 +0200

    fixed the issue with precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,7 @@ module SPIBraviaApp
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += %w( polyfills.js )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
