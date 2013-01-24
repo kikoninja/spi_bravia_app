5efc9d5e1e3a1de481ac349b12f4356234d831ac
Bojan Nikolov <nikolovbojan@gmail.com>
2013-01-24 13:53:26 +0100

    Removed InvUserRole and Role. Added Publisher model

--- app/models/inv_user_role.rb
+++
@@ -1,7 +0,0 @@
-class InvUserRole < ActiveRecord::Base
-  use_connection_ninja(:controlcenter)
-
-  belongs_to :inv_users
-  belongs_to :roles
-  
-end
--- app/models/role.rb
+++
@@ -1,6 +0,0 @@
-class Role < ActiveRecord::Base
-  use_connection_ninja(:controlcenter)
-
-  has_many :inv_user_roles, :through => :inv_users
-
-end
