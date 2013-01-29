63c0f78ab5edb1371701cd8832e63d85bcde1c67
Bojan Nikolov <nikolovbojan@gmail.com>
2013-01-29 23:43:21 +0100

    Deleted add_region_ref_to_publishers migration

--- db/migrate/20130129211751_add_region_ref_to_publishers.rb
+++
@@ -1,5 +0,0 @@
-class AddRegionRefToPublishers < ActiveRecord::Migration
-  def change
-    add_column :publishers, :region_ref, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130129211751) do
+ActiveRecord::Schema.define(:version => 20130129223405) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(:version => 20130129211751) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "ancestry"
+    t.integer  "region_ref"
   end
 
   add_index "categories", ["ancestry"], :name => "index_categories_on_ancestry"
