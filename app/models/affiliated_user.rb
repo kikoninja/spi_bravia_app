aec177078e617c341dc637c96981a90e8255d16b
Peco Danajlovski <peco@invideous.com>
2013-02-06 14:33:59 +0100

    simple routine for checking if user has paid for package

--- app/models/affiliated_user.rb
+++ app/models/affiliated_user.rb
@@ -1,3 +1,8 @@
 class AffiliatedUser < ActiveRecord::Base
   attr_accessible :suit, :username
+
+  def has_access_to_package(package_id)
+    inv_user = InvUser.where(username: username)
+    ConsumerPaidPackage.check_paid_for(inv_user.id, package_id)
+  end
 end
