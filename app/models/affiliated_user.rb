eec7250239f002dfb8aedbd1c8a8d6331aa579f1
Bojan Nikolov <nikolovbojan@gmail.com>
2013-02-26 16:27:41 +0100

    Changed username: username to username.username

--- app/models/affiliated_user.rb
+++ app/models/affiliated_user.rb
@@ -3,7 +3,7 @@ class AffiliatedUser < ActiveRecord::Base
 
   def has_access_to_package(package_id)
     logger.info("Checking access for username: #{username}")
-    inv_user = InvUser.where(username: username).first
+    inv_user = InvUser.where(username: username.username).first
     ConsumerPaidPackage.check_paid_for(inv_user.id, package_id)
   end
 end
