79fbfe2cdc81f0765fdab65ea0177e8aa31497e8
Bojan Nikolov <nikolovbojan@gmail.com>
2013-02-26 16:11:03 +0100

    Put logger.info two lines above :)

--- app/models/affiliated_user.rb
+++ app/models/affiliated_user.rb
@@ -2,8 +2,8 @@ class AffiliatedUser < ActiveRecord::Base
   attr_accessible :suit, :username
 
   def has_access_to_package(package_id)
+    logger.info("Checking access for username: #{username}")
     inv_user = InvUser.where(username: username).first
     ConsumerPaidPackage.check_paid_for(inv_user.id, package_id)
-    logger.info("Checking access for username: #{username}")
   end
 end
